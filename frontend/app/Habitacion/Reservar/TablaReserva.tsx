b5260eb1f2e0ae44ef8a723a4967ad8bbdcb83d0
Lucas <lucasmaglianesi@gmail.com>
2025-11-29 20:00:33 -0300

    feat(frontend): se mejora el funcionamiento de la tabla para el CU05 y se arreglan varios bugs en ella.
    Ahora se puede hacer click derecho en cualquier momento para eliminar la seleccion actual, se puede hacer click en un extremo de seleccion para editarlo y se puede eliminar una seleccion completa dandole click derecho. Ademas ahora solo se permite tener una seleccion en curso.

--- frontend/app/Habitacion/Reservar/TablaReserva.tsx
+++ frontend/app/Habitacion/Reservar/TablaReserva.tsx
@@ -22,6 +22,7 @@ export default function TablaReserva() {
 
 
     const [seleccionadas, setSeleccionadas] = useState(new Map<number, Array<Array<Date>>>());
+    const [indiceSeleccionActual, setIndiceSeleccionActual] = useState<number | null>(null);
 
     function getKey(key: number, map: Map<number, Array<Array<Date>>>) {
         return map.get(key) || [[]];
@@ -33,54 +34,117 @@ export default function TablaReserva() {
     }
 
     console.log(seleccionadas);
+    console.log("indice", indiceSeleccionActual);
     function handleClick(date: Date, habitacion: number) {
-
-        setSeleccionadas((seleccionadas: Map<number, Array<Array<Date>>>) => {
-            const arr = getKey(habitacion, seleccionadas)
-            const newMap = new Map<number, Array<Array<Date>>>(seleccionadas);
-            const index = arr.findIndex(x => x[0]?.getTime() == date.getTime() || x[1]?.getTime() == date.getTime());
-            console.log('size', arr[arr.length - 1].length);
-            if (index > -1) {
-                if(arr.filter(selection => selection.length == 1).length < 1 || arr[index].length == 1) {
-                    if(arr[index][0].getTime() === date.getTime() && arr[index][1]){
-                        console.log("ENTRO 1");
-                        return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][1]], ...arr.slice(index + 1)])
-                    }else if(arr[index][1]?.getTime() === date.getTime()){
-                        console.log("ENTRO 2");
-                        return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][0]], ...arr.slice(index + 1)])
-                    }else if(arr.length > 1){
-                        console.log("ENTRO 3");
-                        return newMap.set(habitacion, [...arr.slice(0, index), ...arr.slice(index + 1)])
-                    }else return newMap.set(habitacion, [[]])
-                }else{
-                    console.log("ENTRO 4");
-                    return seleccionadas
-                }
-
-            }else{
-                console.log("ENTRO AÑADIR");
-                console.log(arr[arr.length - 1][0], arr[arr.length - 1][1])
-                if(!arr.some(selection => selection[0] <= date && selection[1] >= date
-                    || arr[arr.length - 1].length == 1 && (
-                        selection[0] > date && selection[0] < arr[arr.length - 1][0]
-                    || selection[1] > date && selection[1] < arr[arr.length - 1][0]
-                    || selection[0] > date && selection[1] < arr[arr.length - 1][0]
-                    || selection[1] < date && selection[1] > arr[arr.length - 1][0]))){
-                    if(arr[arr.length - 1].length == 2) {
-                        return newMap.set(habitacion, arr.concat([[date]]))
-                    }else if(arr[arr.length - 1].length == 1){
-                        if(arr[arr.length - 1][0] > date){
-                            return newMap.set(habitacion, [...arr.slice(0, -1), [date, arr[arr.length - 1][0]]]);
+        if(!indiceSeleccionActual || indiceSeleccionActual === habitacion){
+            setSeleccionadas((seleccionadas: Map<number, Array<Array<Date>>>) => {
+                const arr = getKey(habitacion, seleccionadas)
+                const newMap = new Map<number, Array<Array<Date>>>(seleccionadas);
+                const index = arr.findIndex(x =>
+                    x.length === 1);
+                console.log("index", index);
+                if (index > -1) {
+                    if(!arr.some(selection => selection[0] <= date && selection[1] >= date
+                        || selection[0] > date && selection[0] < arr[index][0]
+                        || selection[1] > date && selection[1] < arr[index][0]
+                        || selection[0] > date && selection[1] < arr[index][0]
+                        || selection[1] < date && selection[1] > arr[index][0])){
+                        if(indiceSeleccionActual) {
+                            setIndiceSeleccionActual(null)
+                        }else setIndiceSeleccionActual(habitacion)
+                        console.log("SISISI")
+                        if(arr[index][0] > date){
+                            return newMap.set(habitacion, [...arr.slice(0, index), [date, arr[index][0]], ...arr.slice(index + 1)]);
                         }else {
-                            return newMap.set(habitacion, [...arr.slice(0, -1), [arr[arr.length - 1][0], date]]);
+                            return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][0], date], ...arr.slice(index + 1)]);
                         }
-                    }else if(arr[arr.length - 1].length == 0) {
-                        return newMap.set(habitacion, [[date]])
+                    }else return seleccionadas;
+                }else {
+                    const index = arr.findIndex(x => x[0]?.getTime() == date.getTime() || x[1]?.getTime() == date.getTime());
+                    if(index > -1) {
+                        if(arr[index][0].getTime() === date.getTime() && arr[index][1]){
+                            setIndiceSeleccionActual(habitacion)
+                            return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][1]], ...arr.slice(index + 1)])
+                        }else if(arr[index][1]?.getTime() === date.getTime()){
+                            setIndiceSeleccionActual(habitacion)
+                            return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][0]], ...arr.slice(index + 1)])
+                        }else return seleccionadas;
+                    }else if(!arr.some(selection => selection[0] <= date && selection[1] >= date
+                        || arr[arr.length - 1].length == 1 && (
+                            selection[0] > date && selection[0] < arr[arr.length - 1][0]
+                            || selection[1] > date && selection[1] < arr[arr.length - 1][0]
+                            || selection[0] > date && selection[1] < arr[arr.length - 1][0]
+                            || selection[1] < date && selection[1] > arr[arr.length - 1][0]))) {
+                        if(indiceSeleccionActual) {
+                            setIndiceSeleccionActual(null)
+                        }else setIndiceSeleccionActual(habitacion)
+                        if (arr[arr.length - 1].length == 2) {
+                            return newMap.set(habitacion, arr.concat([[date]]))
+                        } else if (arr[arr.length - 1].length == 1) {
+                            if (arr[arr.length - 1][0] > date) {
+                                return newMap.set(habitacion, [...arr.slice(0, -1), [date, arr[arr.length - 1][0]]]);
+                            } else {
+                                return newMap.set(habitacion, [...arr.slice(0, -1), [arr[arr.length - 1][0], date]]);
+                            }
+                        } else {
+                            return newMap.set(habitacion, [...arr.slice(0, -1), [date]])
+                        }
+                    }else return seleccionadas;
+                }
+            });
+        }
+    }
+
+    function handleRightClick(date: Date, habitacion: number) {
+        console.log("ELIMINAR indice", habitacion, indiceSeleccionActual);
+        setSeleccionadas((seleccionadas: Map<number, Array<Array<Date>>>) => {
+            const newMap = new Map<number, Array<Array<Date>>>(seleccionadas);
+
+            if(indiceSeleccionActual === null){
+                const arr = getKey(habitacion, seleccionadas)
+                const index = arr.findIndex(x => x[0]?.getTime() <= date.getTime() && (!x[1] || x[1]?.getTime() >= date.getTime()))
+                if (index > -1) {
+                    console.log("ENTRO IF")
+                    setIndiceSeleccionActual(null)
+                    if(arr.length > 1) {
+                        return newMap.set(habitacion, [...arr.slice(0, index), ...arr.slice(index + 1)])
                     }else{
-                        console.log('ENTRO', arr);
-                        return newMap.set(habitacion, arr.concat([date]))
+                        return newMap.set(habitacion, [[]])
                     }
+                    // if(arr[index][0].getTime() === arr[index][1]?.getTime()) {
+                    //     setIndiceSeleccionActual(null)
+                    //     if(arr.length > 1) {
+                    //         return newMap.set(habitacion, [...arr.slice(0, index), ...arr.slice(index + 1)])
+                    //     }else{
+                    //         return newMap.set(habitacion, [[]])
+                    //     }
+                    //
+                    // }else {
+                    //     if(arr[index][0].getTime() === date.getTime() && arr[index][1]){
+                    //         setIndiceSeleccionActual(habitacion)
+                    //         return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][1]], ...arr.slice(index + 1)])
+                    //     }else if(arr[index][1]?.getTime() === date.getTime()){
+                    //         setIndiceSeleccionActual(habitacion)
+                    //         return newMap.set(habitacion, [...arr.slice(0, index), [arr[index][0]], ...arr.slice(index + 1)])
+                    //     }else {
+                    //         setIndiceSeleccionActual(null)
+                    //         if(arr.length > 1){
+                    //             return newMap.set(habitacion, [...arr.slice(0, index), ...arr.slice(index + 1)])
+                    //         }else return newMap.set(habitacion, [[]])
+                    //     }
+                    // }
+
                 }else return seleccionadas
+            }else {
+                habitacion = indiceSeleccionActual
+                console.log("else")
+                const arr = getKey(habitacion, seleccionadas)
+                const index = arr.findIndex(x => x.length === 1)
+                console.log("ELSE LOG", arr, arr[index])
+                setIndiceSeleccionActual(null)
+                if(arr.length > 1){
+                    return newMap.set(habitacion, [...arr.slice(0, index), ...arr.slice(index + 1)])
+                }else return newMap.set(habitacion, [[]])
             }
         });
     }
@@ -103,10 +167,16 @@ export default function TablaReserva() {
                                 <th>{date.toLocaleDateString("en-GB")}</th>
                                 {habitaciones.map((h, i) => <td key={date.toLocaleDateString("en-GB") + i}>
                                     <TableButton date={date} habitacion={h}
-                                                 onClick={handleClick} seleccionado={getKey(h, seleccionadas).some((arr: Array<Date>) =>
+                                                 onClick={handleClick} onRightClick={handleRightClick}
+                                                 seleccionado={getKey(h, seleccionadas).some((arr: Array<Date>) =>
                                         arr[0]?.getTime() == date.getTime() || (arr[0] <= date && arr[1] >= date))}
                                                  start={getKey(h, seleccionadas).some((arr: Array<Date>) => arr[0]?.getTime() == date.getTime())}
-                                                 end={getKey(h, seleccionadas).some((arr: Array<Date>) => arr[1]?.getTime() == date.getTime())} />
+                                                 end={getKey(h, seleccionadas).some((arr: Array<Date>) => arr[1]?.getTime() == date.getTime())}
+                                                 estado={(date.getDate()*h)%3==0
+                                                     ? 'disponible'
+                                                     : (date.getDate()*h)%3==1
+                                                         ? 'ocupado'
+                                                         : 'reservado'} />
                                 </td>)}
                             </tr>)}
                             </tbody>
--- frontend/app/Habitacion/Reservar/TableButton.tsx
+++ frontend/app/Habitacion/Reservar/TableButton.tsx
@@ -2,11 +2,16 @@
 
 import './ReservaHabitacion.css'
 
-export default function TableButton( {start = false, end = false, estado = 'disponible', date, habitacion, seleccionado = false, onClick}
-                                     : {start?: boolean, end?: boolean, estado?: string, date: Date, habitacion: number, seleccionado?: boolean, onClick: any} ) {
+export default function TableButton( {start = false, end = false, estado = 'disponible', date, habitacion, seleccionado = false, onClick, onRightClick }
+                                     : {start?: boolean, end?: boolean, estado?: string, date: Date, habitacion: number, seleccionado?: boolean, onClick: any, onRightClick: any} ) {
     return (
         <div>
-            <button className='tableButton' data-estado={estado} data-seleccionado={seleccionado} data-start={start} data-end={end} onClick={() => onClick(date, habitacion)} />
+            <button className='tableButton' data-estado={estado} data-seleccionado={seleccionado} data-start={start} data-end={end}
+                    onClick={() => onClick(date, habitacion)} onContextMenu={(e) => {
+                        e.preventDefault(); // Prevent the normal behavior of the click
+                        e.stopPropagation(); // Stop the event from propagating
+                        onRightClick(date, habitacion);
+            }} />
         </div>
     )
 }

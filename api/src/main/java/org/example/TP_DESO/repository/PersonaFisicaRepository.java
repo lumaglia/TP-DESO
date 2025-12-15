631de01fded35f3694960cf6ecee5a1c2950b1d7
Juan Ignacio Alaguibe <juanialaguibe@gmail.com>
2025-12-15 04:11:54 -0300

    feature(cu12:extends): se planteo el caso de uso 13 ya que el 7 lo extiende. Se necesita aun usar el gestor para diferenciar entre personas fisicas y juridicas (ni idea de como es eso (nacho manejalo vos))

---
+++ api/src/main/java/org/example/TP_DESO/repository/PersonaFisicaRepository.java
@@ -0,0 +1,10 @@
+package org.example.TP_DESO.repository;
+
+import org.example.TP_DESO.domain.PersonaFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PersonaFisicaRepository extends JpaRepository<PersonaFisica, Long> {
+
+}
---
+++ frontend/app/ResponsablePago/Alta/page.tsx
@@ -0,0 +1,93 @@
+'use client'
+
+import React, { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form'
+import Encabezado from '../../Encabezado'
+import Row from '../../Row'
+import Campo from '../../Campo'
+import { FormResponsablePago, validation} from '../../../public/constants'
+import {AlertaCancelar, AlertaDocumento} from "../../Alertas";
+import { useRouter, useSearchParams } from 'next/navigation'
+
+export default function AltaResponsablePago(){
+    const form = useForm<FormResponsablePago>();
+    const { register, control, handleSubmit, formState, watch, clearErrors, trigger } = form;
+    const { errors } = formState;
+    const [ alertaCancelarOpen, setAlertaCancelarOpen] = useState(false);
+    const [ alertaDocumentoOpen, setAlertaDocumentoOpen] = useState(false);
+    const formRef = useRef<FormResponsablePago>(null);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const returnTo = searchParams.get('returnTo');
+
+    const onSubmit = (data: FormResponsablePago) => {
+        fetch('http://localhost:8081/ResponsablePago/Alta', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if (res.status === 409) {
+                setAlertaDocumentoOpen(true);
+            }
+            else if (res.ok) {
+                router.push(
+                    `/ResponsablePago/Alta/success?responsablePago=${encodeURIComponent(data.razonSocial)}&returnTo=${encodeURIComponent(returnTo ?? '/')}`
+                );
+            }
+        });
+    };
+
+
+    return (
+        <>
+            <Encabezado titulo={"Dar de alta Responsable de Pago"} />
+            <h2 style={{textAlign:'center'}}>Ingresar los datos del nuevo responsable de pago</h2>
+
+            <form method='post' onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'flex-start'}}>
+                    <div style={{display: 'inline-flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'start'}}>
+                        <Row>
+                            <Campo field='Razon Social' placeholder='' isRequired={true} register={register}
+                                   errors={errors} validation={validation['razonSocial']}/>
+                            <Campo field='CUIT' placeholder='11 - 11222333 - 2' register={register} errors={errors}
+                                   validation={validation['cuil']}
+                                   isRequired/>
+                        </Row>
+                        <Row>
+                            <Campo field='Telefono' placeholder='+54 9 xxxx xxxx' isRequired={true}
+                                   validation={validation['numeroTelefono']} register={register} errors={errors}/>
+                        </Row>
+                        <Row>
+                            <Campo field='Domicilio' placeholder='Calle y Numero' isRequired={true}
+                                   validation={validation['domicilio']} register={register} errors={errors}/>
+                            <Campo field='Departamento' placeholder='Depto y/o Nro de piso'
+                                   validation={validation['departamento']} register={register} errors={errors}/>
+                        </Row>
+                        <Row>
+                            <Campo field='Código Postal' placeholder='Ej. 3252' isRequired={true}
+                                   validation={validation['codigoPostal']} register={register} errors={errors}/>
+                            <Campo field='Localidad' placeholder='Ej. Villa Clara' isRequired={true}
+                                   validation={validation['localidad']} register={register} errors={errors}/>
+                        </Row>
+                        <Row>
+                            <Campo field='Provincia' placeholder='Ej. Entre Ríos' isRequired={true}
+                                   validation={validation['provincia']} register={register} errors={errors}/>
+                            <Campo field='Pais' placeholder='Ej. Argentina' isRequired={true}
+                                   validation={validation['pais']} register={register} errors={errors}/>
+                        </Row>
+                        <Row>
+                            <button type='button' className='Button' onClick={() => setAlertaCancelarOpen(true)}>Cancelar</button>
+                            <button type='submit' className='Button'>Enviar</button>
+                        </Row>
+                    </div>
+                </div>
+            </form>
+
+            <AlertaCancelar open={alertaCancelarOpen} setOpen={setAlertaCancelarOpen} text='el alta de huesped'/>
+            <AlertaDocumento open={alertaDocumentoOpen} setOpen={setAlertaDocumentoOpen} data={formRef.current}/>
+        </>
+    )
+}

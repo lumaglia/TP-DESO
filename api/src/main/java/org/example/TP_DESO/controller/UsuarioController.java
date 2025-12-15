9429911b4b97497519ecb2fb6b5b99a854cff4a2
Lucas <lucasmaglianesi@gmail.com>
2025-12-15 04:40:18 -0300

    feat(api): añade Spring Security, autenticacion y manejo de sesion con tokens JWT

--- api/src/main/java/org/example/TP_DESO/controller/UsuarioController.java
+++ api/src/main/java/org/example/TP_DESO/controller/UsuarioController.java
@@ -6,30 +6,78 @@ import org.example.TP_DESO.service.GestorUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/auth")
 public class UsuarioController {
 
-    GestorUsuario gestorUsuario;
+    private final JwtEncoder jwtEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final GestorUsuario gestorUsuario;
 
     @Autowired
-    public UsuarioController(GestorUsuario gestorUsuario) {
+    public UsuarioController(JwtEncoder jwtEncoder, AuthenticationManager authenticationManager,  GestorUsuario gestorUsuario) {
+        this.jwtEncoder = jwtEncoder;
+        this.authenticationManager = authenticationManager;
         this.gestorUsuario = gestorUsuario;
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @PostMapping("/Alta/Usuario")
-    public ResponseEntity<UsuarioDTO> altaHuesped(@RequestBody UsuarioDTO usuarioDTO) {
-        try {
-            gestorUsuario.altaUsuario(usuarioDTO.getUsuario(), usuarioDTO.getContrasenna());
-        } catch (FracasoOperacion e) {
-            throw new RuntimeException(e);
-        }
-        return ResponseEntity.ok(usuarioDTO);
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody UsuarioDTO usuarioDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getUsuario(), usuarioDTO.getContrasenna())
+        );
+
+        String jwt = generarJWT(usuarioDTO.getUsuario());
+
+        // 4. RESPUESTA
+        Map<String, String> response = new HashMap<>();
+        response.put("token", jwt);
+        return ResponseEntity.ok(response);
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> regisrarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        try {
+            gestorUsuario.altaUsuario(usuarioDTO.getUsuario(), usuarioDTO.getContrasenna());
+            String jwt = generarJWT(usuarioDTO.getUsuario());
+
+            Map<String, String> response = new HashMap<>();
+            response.put("token", jwt);
+            return ResponseEntity.status(201).body(response);
+        } catch (FracasoOperacion e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    private String generarJWT(String usuario){
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(usuario)
+                .claim("scope", "ROLE_USER")
+                .build();
+
+        var encoderParameters = JwtEncoderParameters.from(
+                JwsHeader.with(MacAlgorithm.HS256).build(),
+                claims
+        );
+
+        return jwtEncoder.encode(encoderParameters).getTokenValue();
+
+    }
 }
--- api/src/main/java/org/example/TP_DESO/service/GestorUsuario.java
+++ api/src/main/java/org/example/TP_DESO/service/GestorUsuario.java
@@ -6,14 +6,21 @@ import org.example.TP_DESO.dto.UsuarioDTO;
 import org.example.TP_DESO.domain.Usuario;
 import org.example.TP_DESO.exceptions.FracasoOperacion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GestorUsuario {
+public class GestorUsuario implements UserDetailsService {
     private static GestorUsuario singleton_instance;
 
     @Autowired
     private UsuarioDAOMySQL dao;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public GestorUsuario() throws FracasoOperacion {
     }
@@ -23,8 +30,25 @@ public class GestorUsuario {
         return singleton_instance;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            UsuarioDTO usuario = dao.ObtenerUsuario(new UsuarioDTO(username, ""));
+            if(usuario == null) {
+                throw new UsernameNotFoundException("Usuario no encontrado: " + username);
+            }
+            return User.builder()
+                    .username(usuario.getUsuario())
+                    .password(usuario.getContrasenna())
+                    .roles("USER")
+                    .build();
+        } catch (FracasoOperacion e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void altaUsuario(String usuario, String contrasenna) throws FracasoOperacion {
-        Usuario u = new Usuario(usuario, contrasenna);
+        Usuario u = new Usuario(usuario, passwordEncoder.encode(contrasenna));
         dao.CrearUsuario(u);
     }
 

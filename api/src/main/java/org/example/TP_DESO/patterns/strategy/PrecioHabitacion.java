3832845c5ba2ff2e742b4d744aba3fe299c56827
Nacho <ignaciozermatten@gmail.com>
2025-12-17 21:58:00 -0300

    fix(patron strategy)

--- api/src/main/java/org/example/TP_DESO/patterns/strategy/PrecioHabitacion.java
+++ api/src/main/java/org/example/TP_DESO/patterns/strategy/PrecioHabitacion.java
@@ -4,16 +4,27 @@ import org.example.TP_DESO.dto.HabitacionDTO;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
 @Component
 public class PrecioHabitacion implements PrecioHabitacionStrategy {
-    @Override
-    public double calcularPrecio(HabitacionDTO h, LocalDate inicio, LocalDate fin) {
-        double precio = h.getPrecioNoche();
 
-        long noches = ChronoUnit.DAYS.between(inicio, fin);
+    public double calcularPrecio(
+            HabitacionDTO h,
+            LocalDate inicio,
+            LocalDateTime fin
+    ) {
+        double precioNoche = h.getPrecioNoche();
+        LocalDate fechaFin = fin.toLocalDate();
+        long noches = ChronoUnit.DAYS.between(inicio, fechaFin);
+        double total = precioNoche * noches;
 
-        return precio * noches;
+        if (fin.toLocalTime().isAfter(LocalTime.of(10, 0))) {
+            total += precioNoche / 2;
+        }
+
+        return total;
     }
 }
